5cf46a50b29824e5c606ddb57db8347629ce3209
raka <rkrai059@gmail.com>
2025-09-14 05:24:28 +0530

    Move weather API key to application properties
    Refactored WeatherService to inject the API key from application.properties using @Value, improving security and configurability. The hardcoded API key was removed from the source code and added to the properties file.

--- src/main/java/com/example/MyFirstProject/service/WeatherService.java
+++ src/main/java/com/example/MyFirstProject/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.example.MyFirstProject.service;
 
 import com.example.MyFirstProject.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,15 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
-    private static final String API_KEY = "192dc1d569020d1dd4ff4c1c0a074f5d";
-    private static final String API_URL = "https://api.weatherstack.com/current?access_key=" + API_KEY + "&query={city}";
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     @Autowired
     private RestTemplate restTemplate;
 
     public WeatherResponse getWeather(String city) {
-        String finalAPI = API_URL.replace("{city}", city);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+        String url = "https://api.weatherstack.com/current?access_key=" + apiKey + "&query=" + city;
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(url, HttpMethod.GET, null, WeatherResponse.class);
         return response.getBody();
     }
 }
